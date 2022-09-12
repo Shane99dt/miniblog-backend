4bdc4fcf6c7a8de04d39c7fb6f8675d9c46ff30a
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-12 20:53:49 +0200

    backend live try

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const morgan = require('morgan')
-const cors = require('cors')
-const categoriesRoutes = require('./routes/categories')
-const articlesRoutes = require('./routes/articles')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 5000;
+const morgan = require("morgan");
+const cors = require("cors");
+const categoriesRoutes = require("./routes/categories");
+const articlesRoutes = require("./routes/articles");
 
-app.use(cors())
-app.use(morgan('tiny'))
-app.use(express.json())
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
 
-app.use('/categories', categoriesRoutes)
-app.use('/articles', articlesRoutes)
+app.use("/categories", categoriesRoutes);
+app.use("/articles", articlesRoutes);
 
-app.listen(port, (req, res) =>{
-  console.log(`running on port ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`running on port ${port}`);
+});
